8fc22d305e7f7b1b9fd3d0893f8b504f125b596d
aplki <aplki@protonmail.com>
2021-12-14 12:33:50 +0800

    Chore: Added more cases, beautify return

--- tasks/helpers/halo-helpers/lendingpool-action.ts
+++ tasks/helpers/halo-helpers/lendingpool-action.ts
@@ -41,32 +41,65 @@ task('external:lendingpool-action', 'Initialize incentives controller')
     const lendingPool = await getLendingPool(marketConfigs.HaloConfig.LendingPool[network]);
     const TEST_AMOUNT = parseEther(amount);
     const token = await getIErc20Detailed(TEST_ASSET);
+    const TOKEN_NAME = await token.name();
 
     switch (action) {
       case 'approveToken':
         await token.approve(lendingPool.address, ethers.constants.MaxUint256);
-        console.log(`${await token.name()} approved spend in lending market!`);
+        console.log(`${TOKEN_NAME} approved spend in lending market!`);
         break;
       case 'deposit':
         await lendingPool.deposit(TEST_ASSET, TEST_AMOUNT, DEPLOYER_ADDRESS, 0);
+        console.log('Deposit successful!');
         break;
       case 'withdraw':
         await lendingPool.withdraw(TEST_ASSET, TEST_AMOUNT, DEPLOYER_ADDRESS);
+        console.log('Withdraw successful!');
         break;
       case 'borrow':
         await lendingPool.borrow(TEST_ASSET, TEST_AMOUNT, 2, 0, DEPLOYER_ADDRESS);
+        console.log('Borrow successful!');
         break;
       case 'repay':
         await lendingPool.repay(TEST_ASSET, TEST_AMOUNT, 2, DEPLOYER_ADDRESS);
+        console.log('Repay successful!');
         break;
       case 'getUserReserveData':
-        console.log(await lendingPool.getReserveData(TEST_ASSET));
+        const reserveData = await lendingPool.getReserveData(TEST_ASSET);
+        console.log(`
+        Reserve Data for ${TOKEN_NAME}: 
+        liquidityIndex: ${reserveData.liquidityIndex},
+        variableBorrowIndex: ${reserveData.variableBorrowIndex},
+        currentLiquidityRate: ${reserveData.currentLiquidityRate},
+        currentVariableBorrowRate: ${reserveData.currentVariableBorrowRate},
+        currentStableBorrowRate: ${reserveData.currentStableBorrowRate},
+        lastUpdateTimestamp: ${reserveData.lastUpdateTimestamp},
+        aTokenAddress: ${reserveData.aTokenAddress},
+        stableDebtTokenAddress: ${reserveData.stableDebtTokenAddress},
+        variableDebtTokenAddress: ${reserveData.variableDebtTokenAddress}',
+        interestRateStrategyAddress: ${reserveData.interestRateStrategyAddress},
+        id: ${reserveData.id}
+        `);
         break;
       case 'getReservesList':
         console.log(await lendingPool.getReservesList());
         break;
       case 'setUserUseReserveAsCollateral':
         await lendingPool.setUserUseReserveAsCollateral(TEST_ASSET, true);
+        console.log(`${TOKEN_NAME} set as collateral!`);
+      case 'getUserAccountData':
+        const accountData = await lendingPool.getUserAccountData(DEPLOYER_ADDRESS);
+        console.log(
+          `
+          Account Data for ${DEPLOYER_ADDRESS}:
+          totalCollateralETH: ${accountData.totalCollateralETH}, 
+          totalDebtEth: ${accountData.totalDebtETH}, 
+          availableBorrowsETH: ${accountData.availableBorrowsETH}, 
+          currentLiquidationThreshold: ${accountData.currentLiquidationThreshold}, L
+          TV: ${accountData.ltv}, 
+          healthfactor: ${accountData.healthFactor}`
+        );
+        break;
       default:
         console.log('action not found');
         break;
