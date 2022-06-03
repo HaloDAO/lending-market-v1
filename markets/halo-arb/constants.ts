16f7479039242e73d0c16cd2aec47370d38f1362
Tracy <teresa.arciaga@gmail.com>
2022-06-03 18:26:00 +0800

    Remove configs for other networks

--- markets/halo-arb/constants.ts
+++ markets/halo-arb/constants.ts
@@ -5,30 +5,21 @@ export const HALO_CONTRACT_ADDRESSES = {
   [eEthereumNetwork.buidlerevm]: {},
   [eEthereumNetwork.hardhat]: {},
   [eEthereumNetwork.coverage]: {},
-  [eEthereumNetwork.kovan]: {
-    rewardToken: '0xCffb28605165012cc1e334336c72143eA1a8f47d',
+  [eEthereumNetwork.kovan]: {},
+  [eEthereumNetwork.ropsten]: {},
+  [eEthereumNetwork.main]: {},
+  [eEthereumNetwork.tenderly]: {},
+  [eArbitrumNetwork.arbitrum]: {
+    rewardToken: '0x323C11843DEaEa9f13126FE33B86f6C5086DE138',
     emissionManager: '0x235A2ac113014F9dcb8aBA6577F20290832dDEFd',
     lendingPoolAddress: '',
-    rnbw: '0x518D2efa638c91e15b80Db646cb9A9dB5E976f84',
-    xrnbw: '0xCffb28605165012cc1e334336c72143eA1a8f47d',
-    curveFactory: '0x509c2a6a4F847b30f600230097bc8e50146BC757',
-    usdc: '0x4B466AeAa9c5f639fE7eA5A4692e9ca34afD9CC6',
-    usdcRnbwPair: '0x509c2a6a4F847b30f600230097bc8e50146BC757',
-    fallbackPriceOracle: ZERO_ADDRESS,
+    rnbw: '0xA4b7999A1456A481FB0F2fa7E431b9B641A00770',
+    xrnbw: '0x323C11843DEaEa9f13126FE33B86f6C5086DE138',
+    curveFactory: '0x972127aFf8e6464e50eFc0a2aD344063355AE424',
+    usdc: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+    usdcRnbwPair: '', // sushi
+    fallbackPriceOracle: '0x0000000000000000000000000000000000000000', // Aave's fallbackOracle in Arbitrum is 0x0 (zero address)
   },
-  [eEthereumNetwork.ropsten]: {},
-  [eEthereumNetwork.main]: {
-    rewardToken: '0x47BE779De87de6580d0548cde80710a93c502405',
-    emissionManager: '0x235A2ac113014F9dcb8aBA6577F20290832dDEFd',
-    lendingPoolAddress: '0xC73b2c6ab14F25e1EAd3DE75b4F6879DEde3968E',
-    rnbw: '0xe94b97b6b43639e238c851a7e693f50033efd75c',
-    xrnbw: '0x47BE779De87de6580d0548cde80710a93c502405',
-    curveFactory: '0xFA505d02269bF4Ea59355a4e37fBd882122717e5',
-    usdc: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    usdcRnbwPair: '0x2bcfd3c474484a7b07ca616d70a36c184bbd7ef0', // sushi
-    fallbackPriceOracle: '0x5B09E578cfEAa23F1b11127A658855434e4F3e09',
-  },
-  [eEthereumNetwork.tenderly]: {},
   [eArbitrumNetwork.arbitrumRinkeby]: {
     rewardToken: '0xfbBf11Ae3E8A4b6D9C866B3f16741D1641ccc4d5',
     emissionManager: '0x235A2ac113014F9dcb8aBA6577F20290832dDEFd',
--- markets/halo-arb/index.ts
+++ markets/halo-arb/index.ts
@@ -33,24 +33,9 @@ export const HaloArbConfig: IHaloConfiguration = {
     [eEthereumNetwork.buidlerevm]: {},
     [eEthereumNetwork.hardhat]: {},
     [eEthereumNetwork.coverage]: {},
-    [eEthereumNetwork.kovan]: {
-      DAI: '0x33988C7f1333773DCCE4c5d28cc4e785a7a07711',
-      WETH: '0x1363b62C9A82007e409876A71B524bD63dDc67Dd',
-      USDC: '0x4B466AeAa9c5f639fE7eA5A4692e9ca34afD9CC6',
-      USDT: '0x98388b94c7bEF52CD361fcf037c7249BB6D4282b',
-      WBTC: '0xeD57b6849762Ead86f71b41eEC743cE261639Aa8',
-    },
+    [eEthereumNetwork.kovan]: {},
     [eEthereumNetwork.ropsten]: {},
-    [eEthereumNetwork.main]: {
-      DAI: '0x6b175474e89094c44da98b954eedeac495271d0f',
-      //TUSD: '0x0000000000085d4780B73119b644AE5ecd22b376',
-      WETH: '0xDD03Fd22858ee6a3fa752c7872fC670Da52B58Da',
-      USDC: '0xE2ea809d18B3AD1ac3839dFa01f8F4870D5ad917',
-      USDT: '0xc1823C66137BF758d1Ac6d2675a62b256a89b143',
-      WBTC: '0x42721E500325A725B29309116496adbBE1AE4a7e',
-      // BUSD: '0x4fabb145d64652a948d72533023f6e7a623c7c53', // To Check
-      // SUSD: '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // To Check
-    },
+    [eEthereumNetwork.main]: {},
     [eEthereumNetwork.tenderly]: {},
     [eArbitrumNetwork.arbitrum]: {
       DAI: '',
